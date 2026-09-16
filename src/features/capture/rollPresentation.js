2fe1953eab6d933e5cbfec5a067feeb06c393b05
Cristian <139021004+Cbaca4@users.noreply.github.com>
2026-09-16 06:23:26 -0500

    test: add Roll presentation stage model

---
+++ src/features/capture/rollPresentation.js
@@ -0,0 +1,15 @@
+export const ROLL_STAGES = [
+  "loading",
+  "winding",
+  "developing",
+  "revealing",
+  "finished",
+];
+
+export function getNextRollStage(stage) {
+  const index = ROLL_STAGES.indexOf(stage);
+  if (index < 0 || index === ROLL_STAGES.length - 1) {
+    return "finished";
+  }
+  return ROLL_STAGES[index + 1];
+}
