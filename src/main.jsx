0586d4d7f53f16f90dd069ff4fd3d3558a53fc2d
Cristian <139021004+Cbaca4@users.noreply.github.com>
2026-09-16 15:57:48 -0500

    Load companion costume alignment overrides

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,6 @@ import React from 'react';
 import {createRoot} from 'react-dom/client';
 import './styles.css';
 import './mobile.css';
+import './features/companion/CompanionCostume.css';
 import FlicdApp from './app/FlicdApp.jsx';
 createRoot(document.getElementById('root')).render(<React.StrictMode><FlicdApp/></React.StrictMode>);
