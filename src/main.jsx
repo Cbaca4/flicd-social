5bd49d5a4ca87bd600621954ee7fd711720ebcb7
Cristian <139021004+Cbaca4@users.noreply.github.com>
2026-09-16 09:58:01 -0500

    style: load responsive mobile overrides

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
 import './styles.css';
+import './mobile.css';
 import FlicdApp from './app/FlicdApp.jsx';
 createRoot(document.getElementById('root')).render(<React.StrictMode><FlicdApp/></React.StrictMode>);
