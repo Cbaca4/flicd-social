bda7c5e26b78c0c54aaa5de9804aee28ce31e630
Cristian <139021004+Cbaca4@users.noreply.github.com>
2026-09-16 06:34:50 -0500

    fix: configure ESLint to parse JSX

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,3 @@
-import js from "@eslint/js";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 
@@ -6,9 +5,17 @@ export default [
   {
     ignores: ["dist/**", "node_modules/**"],
   },
-  js.configs.recommended,
   {
     files: ["**/*.{js,jsx}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
